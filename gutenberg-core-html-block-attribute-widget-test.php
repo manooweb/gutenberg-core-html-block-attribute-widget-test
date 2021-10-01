03bfda75a768744f40ee60730867328d3bbfa3e7
Emmanuel Hesry <ehesry@gmail.com>
2021-10-01 08:47:00 +0200

    Change enqueued script handle

--- gutenberg-core-html-block-attribute-widget-test.php
+++ gutenberg-core-html-block-attribute-widget-test.php
@@ -2,6 +2,6 @@
 function gutenberg_core_html_block_attribute_widget_test_enqueue_script() {
 	$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-	wp_enqueue_script( 'gutenberg_legacy_widgets', WPMU_PLUGIN_URL . '/dist/gutenberg-core-html-block-attribute-widget-test' . $suffix . '.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'gutenberg_core_html_attribute', WPMU_PLUGIN_URL . '/dist/gutenberg-core-html-block-attribute-widget-test' . $suffix . '.js', array(), '1.0.0', true );
 }
 add_action( 'admin_enqueue_scripts', 'gutenberg_core_html_block_attribute_widget_test_enqueue_script' );
